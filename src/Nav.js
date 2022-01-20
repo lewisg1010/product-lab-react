779e578ceaf8aa5d0279475c803e9c291c2b04f9
Galen Lewis <galenlewis@Galens-Air.fios-router.home>
2022-01-20 16:08:47 -0500

    Fixed routing issue

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ import Clients from './Clients';
 import Home from './Home';
 import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Students from './Students';
 import Events from './Events';
 import Blog from './Blog';
@@ -13,13 +13,13 @@ import Faqs from './Faqs';
 import Getinvolved from './Getinvolved';
 import DarkMode from './DarkMode';
 import Post from './Post';
+import ScrollToTop from './ScrollToTop';
 
 
 export default class Navb extends Component {
     
     render() {
         return(
-          // <Container>
             <Router>
               
       <Navbar collapseOnSelect expand="lg" fixed="top" id="nav">
@@ -28,14 +28,14 @@ export default class Navb extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" id="navbartoggle" />
           <Navbar.Collapse className="basic-navbar-nav" id="nogrow">
             <Nav className="me-auto">
-              <Nav.Link eventKey="1" as={Link} to={"/#"} id="navlinkoverridetextcolor" className="underline2">Home</Nav.Link>
-              <Nav.Link eventKey="2" as={Link} to={"/about#"} id="navlinkoverridetextcolor" className="underline2">About</Nav.Link>
-              <Nav.Link eventKey="3" as={Link} to={"/clients#"} id="navlinkoverridetextcolor" className="underline2">Clients</Nav.Link>
-              <Nav.Link eventKey="4" as={Link} to={"/students#"} id="navlinkoverridetextcolor" className="underline2">Students</Nav.Link>
-              <Nav.Link eventKey="5" as={Link} to={"/events#"} id="navlinkoverridetextcolor" className="underline2">Events</Nav.Link>
+              <Nav.Link eventKey="1" as={Link} to={"/"} id="navlinkoverridetextcolor" className="underline2">Home</Nav.Link>
+              <Nav.Link eventKey="2" as={Link} to={"/about"} id="navlinkoverridetextcolor" className="underline2">About</Nav.Link>
+              <Nav.Link eventKey="3" as={Link} to={"/clients"} id="navlinkoverridetextcolor" className="underline2">Clients</Nav.Link>
+              <Nav.Link eventKey="4" as={Link} to={"/students"} id="navlinkoverridetextcolor" className="underline2">Students</Nav.Link>
+              <Nav.Link eventKey="5" as={Link} to={"/events"} id="navlinkoverridetextcolor" className="underline2">Events</Nav.Link>
               <NavDropdown title="Resources" id="navlinkoverridetextcolor" className="underline">
-                <NavDropdown.Item eventKey="6" as={Link} to={"/blog#"} id="navdropdownitem">Blog</NavDropdown.Item>
-                <NavDropdown.Item eventKey="7" as={Link} to={"/faqs#"} id="navdropdownitem">FAQs</NavDropdown.Item>
+                <NavDropdown.Item eventKey="6" as={Link} to={"/blog"} id="navdropdownitem">Blog</NavDropdown.Item>
+                <NavDropdown.Item eventKey="7" as={Link} to={"/faqs"} id="navdropdownitem">FAQs</NavDropdown.Item>
               </NavDropdown>
               <Nav.Link href="/getinvolved"><Button id="button"><span id="bold">Partner with us</span></Button></Nav.Link>
               <DarkMode />
@@ -44,20 +44,20 @@ export default class Navb extends Component {
         </Container>
       </Navbar>
       <div>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about" element={<About />} /> 
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/blog" element={<Blog />} exact />
-        <Route path='/blog/post/:slug' element={<Post />} />
-        <Route path="/faqs" element={<Faqs />} /> 
-        <Route path="/getinvolved" element={<Getinvolved />} /> 
-      </Routes>
+      <ScrollToTop />
+      <Switch>
+        <Route path="/" exact component={Home} /> 
+        <Route path="/about" exact component={About} /> 
+        <Route path="/clients" exact component={Clients} />
+        <Route path="/students" exact component={Students} />
+        <Route path="/events" exact component={Events} />
+        <Route path="/blog" exact component={Blog} />
+        <Route path='/blog/post/:slug' exact component={Post} />
+        <Route path="/faqs" exact component={Faqs} /> 
+        <Route path="/getinvolved" exact component={Getinvolved} /> 
+      </Switch>
       </div>
       </Router>
-      // </Container>
         )
     }
 }
