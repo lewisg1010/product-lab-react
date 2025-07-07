35f53374291548d9b2f7e097a323ff936d9dc358
John Russell <johnandrewrussell@gmail.com>
2025-07-06 20:07:20 -0700

    test

--- src/Bio.js
+++ src/Bio.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-<Card.Img variant="top" src={bio.photo.url} className="card-img" />
+<Card.Img
+  variant="top"
+  src="https://us-east-1.graphassets.com/ANbsyxEKT0KZO3ZL8LdIfz/cmcshi8x3pb0h07ir94hk2rql"
+  style={{ width: "318px", height: "230px", objectFit: "cover" }}
+/>
 
 function Bio({ bios }) {
     const { slug } = useParams();
