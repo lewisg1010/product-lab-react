1331659a3345f2ee24afed93de0d7dfd2b90e578
John Russell <johnandrewrussell@gmail.com>
2025-07-06 20:05:40 -0700

    commit

--- src/Bio.js
+++ src/Bio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-<Card.Img variant="top" src={bio.photo.url} className="card-photo" />
+<Card.Img variant="top" src={bio.photo.url} className="card-img" />
 
 function Bio({ bios }) {
     const { slug } = useParams();
