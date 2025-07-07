5f791066f3b091ee99b589d0abbb16cdb3154cd0
John Russell <johnandrewrussell@gmail.com>
2025-07-06 20:23:03 -0700

    fixed photos

--- src/About.js
+++ src/About.js
@@ -13,150 +13,188 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
 
-
 function About() {
+  const [bios, setBios] = useState(null);
 
-    const [bios, setBios] = useState(null);
-
-    useEffect(() => {
-        const fetchBios = async() => {
-            const { bios } = await request('https://api-us-east-1.graphcms.com/v2/cky85ol262n3s01z42208339l/master', 
-            `{
-            bios {
-                name
-                title
-                photo {
-                url
-                }
-                aboutme
-                board
-                slug
+  useEffect(() => {
+    const fetchBios = async () => {
+      const { bios } = await request(
+        'https://api-us-east-1.graphcms.com/v2/cky85ol262n3s01z42208339l/master',
+        `{
+          bios {
+            name
+            title
+            photo {
+              url
             }
-            }
-            `
-            );
+            aboutme
+            board
+            slug
+          }
+        }`
+      );
 
-    setBios(bios);
-  };
-
-  fetchBios();
-}, []);
-
-
-        return (
-            <div id="container" class="center">
-                {!bios ? (
-                    ''
-                ) : (
-                    <div>
-                    <section>
-                    <div id="divmesomespacesmall"></div>
-                    <Zoom top>
-                    <h1 className="center bold" id="gradienttext">About Product Lab</h1>
-                    </Zoom>
-                    <div id="divmesomespacesmall" />
-                      <p id="textchunk">Launched in September 2021, Product Lab is Harvard College's first Product Management organization.</p>
-                      <p id="textchunk">Through training programs, industry experience, and mentorship, Product Lab helps creative problem solvers from diverse backgrounds grow into future product leaders.</p>
-                  </section>
-                  <section>
-                    <div id="divmesomespacesmall" />
-                    <h1 className="bold">HPL Board Members</h1>
-                    <div id="divmesomespacesmall" />
-                  </section>
-                  
-    <Container fluid id="limitwidth">
-    <Row xs={1} sm={1} md={2} lg={2} xl={3} className="g-3 align-items-center d-flex card-block">
-        {bios.map((bio, i) => (
-    <Fade cascade>
-    <Col key={bio.name}>
-    <Card style={{"height" : '120%', width: '20rem'}} id="aboutcard">
-      <Card.Img variant="top" src={bio.photo.url} />
-      <Card.Body>
-        <Card.Title>
-            <a id="cardtitlefont">{bio.name}</a>
-        </Card.Title>
-        <Card.Text>
-        <p>{bio.title}</p>
-        </Card.Text>
-      </Card.Body>
-      <Accordion id="accordian">
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon id="expandmoreicon" />}
-      aria-controls="panel1a-content"
-      id="panel1a-header"
-    >
-      <Typography id="overridefont">About {bio.name.split(" ")[0]}</Typography>
-    </AccordionSummary>
-    <AccordionDetails>
-      <Typography id="overridefont">
-      {bio.aboutme}
-      </Typography>
-    </AccordionDetails>
-  </Accordion>
-    </Card>
-    <div id="divmesomespace"></div>
-    </Col>
-    </Fade>
-        ))}
-    </Row>
-    </Container>
-                    </div>
-                )}
-                <section id="pasteventfade">
-  <h1 className="bold">Our Advisory Board</h1>
-  <div id="divmesomespacesmall" />
-</section>
-
-<Container fluid id="pasteventfade">
-    <Row xs={1} sm={1} md={2} lg={3} xl={3} className="g-1 align-items-center d-flex card-block">
-        <Col>
-        <Card style={{"height" : '120%', width: '18rem'}} id="advisorcard">
-          <Card.Img variant="top" src={'./tomphoto.jpg'} />
-          <Card.Body>
-            <Card.Title>
-                <a id="cardtitlefont">Tom Eisenmann</a>
-            </Card.Title>
-            <Card.Text>
-            <p>Professor of Entrepreneurship at Harvard Business School & Faculty Chair of Harvard Innovation Lab. Author of &quot;The Fail-Safe Startup&quot;.</p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <div id="divmesomespace"></div>
-        </Col>
-
-        <Col>
-        <Card style={{"height" : '120%', width: '18rem'}} id="advisorcard">
-          <Card.Img variant="top" src={'./deborahphoto.png'} />
-          <Card.Body>
-            <Card.Title>
-                <a id="cardtitlefont">Deborah Liu</a>
-            </Card.Title>
-            <Card.Text>
-            <p>CEO of Ancestry.com & Founder of Women in Product. Previously VP of Product at Facebook & Director of Product at eBay.</p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <div id="divmesomespace"></div>
-        </Col>
-
-        <Col>
-        <Card style={{"height" : '120%', width: '18rem'}} id="advisorcard">
-          <Card.Img variant="top" src={'./jeffphoto.png'} />
-          <Card.Body>
-            <Card.Title>
-                <a id="cardtitlefont">Jeffrey Bussgang</a>
-            </Card.Title>
-            <Card.Text>
-            <p>Senior Lecturer at Harvard Business School, Co-Founder & General Partner at Flybridge Capital Partners, & Co-Founder at Hack.Diversity.</p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <div id="divmesomespace"></div>
-        </Col>
-    </Row>
-    </Container>
-            </div>
-        );
+      setBios(bios);
     };
+
+    fetchBios();
+  }, []);
+
+  return (
+    <div id="container" className="center">
+      {!bios ? (
+        ''
+      ) : (
+        <div>
+          <section>
+            <div id="divmesomespacesmall"></div>
+            <Zoom top>
+              <h1 className="center bold" id="gradienttext">
+                About Product Lab
+              </h1>
+            </Zoom>
+            <div id="divmesomespacesmall" />
+            <p id="textchunk">
+              Launched in September 2021, Product Lab is Harvard College's first Product Management organization.
+            </p>
+            <p id="textchunk">
+              Through training programs, industry experience, and mentorship, Product Lab helps creative problem solvers from diverse backgrounds grow into future product leaders.
+            </p>
+          </section>
+
+          <section>
+            <div id="divmesomespacesmall" />
+            <h1 className="bold">Board Members</h1>
+            <div id="divmesomespacesmall" />
+          </section>
+
+          <Container fluid id="limitwidth">
+            <Row xs={1} sm={1} md={2} lg={2} xl={3} className="g-3 align-items-center d-flex card-block">
+            {[...bios]
+  .sort((a, b) => {
+    const isExec = (person) =>
+      person.title?.toLowerCase().includes('president') ||
+      person.title?.toLowerCase().includes('executive');
+
+    const rank = (person) => {
+      if (isExec(person)) return 2; // highest rank
+      if (person.board) return 1;
+      return 0;
+    };
+
+    return rank(b) - rank(a); // sort descending by rank
+  })
+  .map((bio, i) => (
+
+                <Fade cascade key={bio.slug}>
+                  <Col>
+                    <Card style={{ height: '100%', width: '20rem' }} id="aboutcard">
+                      <div style={{ width: '318px', height: '230px', overflow: 'hidden', margin: 'auto' }}>
+                        <img
+                          src={bio.photo.url}
+                          alt={bio.name}
+                          style={{
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                            display: 'block'
+                          }}
+                        />
+                      </div>
+                      <Card.Body>
+                        <Card.Title>
+                          <a id="cardtitlefont">{bio.name}</a>
+                        </Card.Title>
+                        <Card.Text>
+                          <p>{bio.title}</p>
+                        </Card.Text>
+                      </Card.Body>
+                      <Accordion id="accordian">
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon id="expandmoreicon" />}
+                          aria-controls="panel1a-content"
+                          id="panel1a-header"
+                        >
+                          <Typography id="overridefont">About {bio.name.split(' ')[0]}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography id="overridefont">{bio.aboutme}</Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    </Card>
+                    <div id="divmesomespace"></div>
+                  </Col>
+                </Fade>
+              ))}
+            </Row>
+          </Container>
+        </div>
+      )}
+
+      <section id="pasteventfade">
+        <h1 className="bold">Our Advisory Board</h1>
+        <div id="divmesomespacesmall" />
+      </section>
+
+      <Container fluid id="pasteventfade">
+        <Row xs={1} sm={1} md={2} lg={3} xl={3} className="g-1 align-items-center d-flex card-block">
+          <Col>
+            <Card style={{ height: '100%', width: '18rem' }} id="advisorcard">
+              <Card.Img variant="top" src={'./tomphoto.jpg'} />
+              <Card.Body>
+                <Card.Title>
+                  <a id="cardtitlefont">Tom Eisenmann</a>
+                </Card.Title>
+                <Card.Text>
+                  <p>
+                    Professor of Entrepreneurship at Harvard Business School & Faculty Chair of Harvard Innovation
+                    Lab. Author of "The Fail-Safe Startup".
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <div id="divmesomespace"></div>
+          </Col>
+
+          <Col>
+            <Card style={{ height: '100%', width: '18rem' }} id="advisorcard">
+              <Card.Img variant="top" src={'./deborahphoto.png'} />
+              <Card.Body>
+                <Card.Title>
+                  <a id="cardtitlefont">Deborah Liu</a>
+                </Card.Title>
+                <Card.Text>
+                  <p>
+                    CEO of Ancestry.com & Founder of Women in Product. Previously VP of Product at Facebook & Director
+                    of Product at eBay.
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <div id="divmesomespace"></div>
+          </Col>
+
+          <Col>
+            <Card style={{ height: '100%', width: '18rem' }} id="advisorcard">
+              <Card.Img variant="top" src={'./jeffphoto.png'} />
+              <Card.Body>
+                <Card.Title>
+                  <a id="cardtitlefont">Jeffrey Bussgang</a>
+                </Card.Title>
+                <Card.Text>
+                  <p>
+                    Senior Lecturer at Harvard Business School, Co-Founder & General Partner at Flybridge Capital
+                    Partners, & Co-Founder at Hack.Diversity.
+                  </p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <div id="divmesomespace"></div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
 
 export default About;
